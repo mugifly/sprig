e4a1bd2aa75ee7fa4008401cc76aa79bd42751f3
Masanori Ohgita <mp@ohgita.info>
2013-06-04 17:31:22 +0900

    Add(Connector/Twitter): add crawl method and crawl_single_talk method

--- lib/Sprig/Connector/Twitter.pm
+++ lib/Sprig/Connector/Twitter.pm
@@ -2,7 +2,28 @@ package Spring::Connector::Twitter;
 
 sub new {
 	my $class = shift;
+	my $params = shift;
 	my $self = bless({}, $class);
+
+	# Database parameter
+	$self->{db} = $params->{db};
 	
 	return $self;
+}
+
+sub crawl {
+
+}
+
+sub crawl_single_talk {
+	my ($message_id) = @_;
+
+	
+
+	$self->{db}->set(voice => undef => {
+		text => $text,
+		date => Time::Piece->new(),
+		before_voice_id = 
+	});
+
 }
