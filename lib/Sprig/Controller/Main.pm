0d8be7553c41c8ae1d7e2022f3e6736f4ac7c652
Masanori Ohgita <mp@ohgita.info>
2013-07-18 09:27:32 +0900

    Add(/config): AngularJS

--- lib/Sprig/Controller/Main.pm
+++ lib/Sprig/Controller/Main.pm
@@ -9,6 +9,48 @@ sub top {
 sub config {
 	my $self = shift;
 
+	if ( $self->param('req') eq 'json'  && $self->param('target') eq 'queues' ){
+		# Queues 
+		my @queues = ();
+		my $rows = $self->db->get( queue => { order => { priority => -1 } } );
+		while ( my $r = $rows->next ){
+			push(@queues, $r->{column_values});
+		}
+		$self->render( json => { queues => \@queues } );
+		return;
+	} elsif ( $self->param('req') eq 'json' && $self->param('target') eq 'queue' && $self->param('mode') eq 'add' ){
+		# Queue - Add
+		if($self->param('type') eq 'music'){
+			# Music queue
+			if($self->param('action') eq 'play' && $self->param('source') eq 'youtube' && $self->param('url') =~ /(http|https):\/\/(www\.|)youtube\.com\/watch\?v=(\w+)(|&.*)$/) {
+				# Music (youtube) play
+				$self->db->set( queue => undef => {
+					type => $self->param('type'),
+					action => $self->param('action'),
+					priority => $self->param('priority'),
+					date => Time::Piece->new(),
+					detail => {
+						source => 'youtube', source_id => $3, play_status => 0,
+					},
+					status => 0,
+				});
+				return;
+			} elsif ($self->param('action') eq '') {
+				$self->db->set( queue => undef => {
+					type => $self->param('type'),
+					action => $self->param('action'),
+					priority => 1000,
+					date => Time::Piece->new(),
+					status => 0,
+				});
+				return;
+			}
+		} 
+
+		$self->render( json => { result => "Invalid type." } , status => 400);
+		return;
+	}
+
 	# Social accounts
 	my @social_accounts = ();
 	my $rows = $self->db->get( social_account =>  {  where => [] });
@@ -16,17 +58,9 @@ sub config {
 		push(@social_accounts, $r->{column_values});
 	}
 
-	# Queues
-	my @queues = ();
-	my $rows = $self->db->get( queue => { order => { priority => -1 } } );
-	while ( my $r = $rows->next ){
-		push(@queues, $r->{column_values});
-	}
-
 	# Render
 	$self->render(
 		social_accounts => \@social_accounts,
-		queues => \@queues,
 	);
 }
 
